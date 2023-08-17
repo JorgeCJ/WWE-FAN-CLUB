ca7a4fda6aae96fc8937bdd38ec47653f1cfc207
Jorge de Carvalho <127647774+JorgeCJ@users.noreply.github.com>
2023-08-17 09:21:34 -0300

    LGTM

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,7 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      redirectUri: 'https://wwe-fan-club.vercel.app/api/auth/callback/google',
+      redirectUri: process.env.NEXTAUTH_AUTH,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET
